22ff53f842843e4b9c4c0f39b61e95d30ead7e5f
sanechka <belikalexandra@gmail.com>
2021-02-10 11:40:07 +0300

    wip (tests)

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,7 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Vuetify from 'vuetify'
 
 describe('HelloWorld.vue', () => {
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
